40f029c5d0d12b251cb0c19c6ddcb737b0117dd0
Vibhor Gupta <vibhorgupta@Vibhors-MacBook-Pro.local>
2021-05-06 22:45:11 +0530

    06-alternative-flavor

--- 3-modules.js
+++ 3-modules.js
@@ -2,7 +2,9 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./06-alternative-flavor')
 
+// console.log(data);
 
 sayHi('susan')
 sayHi(names.john)
