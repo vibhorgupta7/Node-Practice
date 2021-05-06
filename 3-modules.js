7da6e3ee999ecc74491cf17bd2da1c744766a721
Vibhor Gupta <vibhorgupta@Vibhors-MacBook-Pro.local>
2021-05-06 22:37:35 +0530

    3-modules, 4-names, 5-utils

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
